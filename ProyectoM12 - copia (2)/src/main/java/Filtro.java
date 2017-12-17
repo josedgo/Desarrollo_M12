5551cb024634231b9bf46362b700691b3875fb22
josedgo <josedgo2014@gmail.com>
2017-12-17 16:47:43 -0400

    Agregarndo Nuevo WS

---
+++ ProyectoM12 - copia (2)/src/main/java/Filtro.java
@@ -0,0 +1,37 @@
+package main.java;
+
+//import java.util.Date;
+
+public class Filtro {
+    private Integer id;
+    private String tipo;
+    private String descripcion;
+
+    public Filtro() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
